570b48899c70abdc217ff59cbc74a1f989f1fd8f
SaumilShah66 <shah.saumil66@gmail.com>
2019-11-18 11:06:03 -0500

    [move.cpp]Cpplint errors removed

--- src/mover.cpp
+++ src/mover.cpp
@@ -36,74 +36,74 @@
 #include "geometry_msgs/Twist.h"
 #include "strolling_turtlebot/mover.hpp"
 
-/// This method reads the laserscan data and checks if they are too much closer to an object
-void Mover::checkProximity(const sensor_msgs::LaserScan::ConstPtr& intensities){
-	ROS_INFO_STREAM("Checking time....");
-	for(auto intensity:intensities->ranges){
-		if(intensity <= thresholdIntensity){
-			thresholdCroseed = true;  /// changes flag if threshold crossed
-			ROS_INFO_STREAM("Danger");
-			break;
-		}
-		else{
-			thresholdCroseed = false;
-		}
-	}
-	ROS_INFO_STREAM(thresholdCroseed);
-	if(!thresholdCroseed){
-		ROS_INFO_STREAM("Bot is safe...Bot can stroll...Enjoy");
-		botMover.publish(moveAhead());  /// Pass message to move ahead
-	}
-	else{
-		ROS_INFO_STREAM("Ooops... Danger... Time to turn");
-		botMover.publish(stopMessage());  /// Pass message to stop the bot
-		botMover.publish(rotateBot());  /// Pass message to rotate the bot
-		ros::Duration(0.5).sleep();   /// Wait for half a second
-		botMover.publish(stopMessage());  /// Pass message to stop the bot 
-	}
-}
+/// This method reads the laserscan data and checks if they are
+/// too much closer to an object
 
+void Mover::checkProximity(
+    const sensor_msgs::LaserScan::ConstPtr& intensities) {
+  ROS_INFO_STREAM("Checking time....");
+  for (auto intensity : intensities->ranges) {
+    if (intensity <= thresholdIntensity) {
+      thresholdCroseed = true;  /// changes flag if threshold crossed
+      ROS_INFO_STREAM("Danger");
+      break;
+    } else {
+      thresholdCroseed = false;
+    }
+  }
+  ROS_INFO_STREAM(thresholdCroseed);
+  if (!thresholdCroseed) {
+    ROS_INFO_STREAM("Bot is safe...Bot can stroll...Enjoy");
+    botMover.publish(moveAhead());  /// Pass message to move ahead
+  } else {
+    ROS_INFO_STREAM("Ooops... Danger... Time to turn");
+    botMover.publish(stopMessage());  /// Pass message to stop the bot
+    botMover.publish(rotateBot());  /// Pass message to rotate the bot
+    ros::Duration(0.5).sleep();   /// Wait for half a second
+    botMover.publish(stopMessage());  /// Pass message to stop the bot
+  }
+}
 
 /// Constructor created
-Mover::Mover(){
-	ROS_INFO_STREAM("Initializing publisher and subscriber");
-	/// Subscriber initialized
-	laserReader = n.subscribe<sensor_msgs::LaserScan>("/scan", 30, &Mover::checkProximity, this);
-	/// Publisher initialized
-	botMover = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity",30);
-	moveMessage.linear.x = 0;   /// Linear velocity in X direction 0
-	moveMessage.linear.y = 0;   /// Linear velocity in Y direction 0
-	moveMessage.linear.z = 0;   /// Linear velocity in Z direction 0
-	moveMessage.angular.x = 0;  /// Angular velocity in X direction 0
-	moveMessage.angular.y = 0;  /// Angular velocity in Y direction 0
-	moveMessage.angular.z = 0;  /// Angular velocity in Z direction 0
+Mover::Mover() {
+  ROS_INFO_STREAM("Initializing publisher and subscriber");
+  /// Subscriber initialized
+  laserReader = n.subscribe < sensor_msgs::LaserScan
+      > ("/scan", 30, &Mover::checkProximity, this);
+  /// Publisher initialized
+  botMover = n.advertise < geometry_msgs::Twist
+      > ("/mobile_base/commands/velocity", 30);
+  moveMessage.linear.x = 0;   /// Linear velocity in X direction 0
+  moveMessage.linear.y = 0;   /// Linear velocity in Y direction 0
+  moveMessage.linear.z = 0;   /// Linear velocity in Z direction 0
+  moveMessage.angular.x = 0;  /// Angular velocity in X direction 0
+  moveMessage.angular.y = 0;  /// Angular velocity in Y direction 0
+  moveMessage.angular.z = 0;  /// Angular velocity in Z direction 0
 }
 
-Mover::~Mover(){
-	ROS_INFO_STREAM("Destructed");
+Mover::~Mover() {
+  ROS_INFO_STREAM("Destructed");
 }
 
-geometry_msgs::Twist Mover::moveAhead(){
-	moveMessage.linear.x = 0.5;  /// Linear velocity in X to move forward
-	return moveMessage;
+geometry_msgs::Twist Mover::moveAhead() {
+  moveMessage.linear.x = 0.5;  /// Linear velocity in X to move forward
+  return moveMessage;
 }
 
-geometry_msgs::Twist Mover::rotateBot(){
-	moveMessage.linear.x = 0.0;   
-	moveMessage.angular.z = rotateAngle;  /// Angular velocity to rotate
-	return moveMessage;
+geometry_msgs::Twist Mover::rotateBot() {
+  moveMessage.linear.x = 0.0;
+  moveMessage.angular.z = rotateAngle;  /// Angular velocity to rotate
+  return moveMessage;
 }
 
-geometry_msgs::Twist Mover::stopMessage(){
-	/// Set all velocities to zero 
-	moveMessage.linear.x = 0;  
-	moveMessage.linear.y = 0;
-	moveMessage.linear.z = 0;
-	moveMessage.angular.x = 0;
-	moveMessage.angular.y = 0;
-	moveMessage.angular.z = 0;
-	return moveMessage;
+geometry_msgs::Twist Mover::stopMessage() {
+  /// Set all velocities to zero
+  moveMessage.linear.x = 0;
+  moveMessage.linear.y = 0;
+  moveMessage.linear.z = 0;
+  moveMessage.angular.x = 0;
+  moveMessage.angular.y = 0;
+  moveMessage.angular.z = 0;
+  return moveMessage;
 }
-
-
 
